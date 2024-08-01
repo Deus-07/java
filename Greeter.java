5adadb5ac8d539dad5a2105bae2eef45825a9725
Deus-07 <mrgovindsahab@gmail.com>
2024-08-01 14:22:22 +0530

    Add files via upload

---
+++ Greeter.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Greeter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+        scanner.close();
+    }
+}
